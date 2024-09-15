da97c953524a7e349aa0591a0f704115c101de20
Grayson Rieger <graysonr12@icloud.com>
2024-09-15 12:36:24 -0500

    document

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -15,7 +15,9 @@ export default function Document() {
 
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <Main />
         <NextScript />
